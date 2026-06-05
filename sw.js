3cb874e465e52d1bacb7f2c2a06b78f5049e715a
Ram Prasath <sreesathyastore@gmail.com>
2026-06-05 21:09:03 +0530

    check

--- sw.js
+++ sw.js
@@ -1,19 +1,24 @@
-const CACHE = 'sss-v1';
-const FILES = [
+const CACHE_NAME = 'sss-cache-v1';
+const FILES_TO_CACHE = [
   '/Sree-sathya-store/sree-sathya-store.html',
   '/Sree-sathya-store/track-order.html',
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request).catch(() =>
-      caches.match(e.request)
-    )
+    fetch(e.request)
+      .catch(() => caches.match(e.request))
   );
 });
